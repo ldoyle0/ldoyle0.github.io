24dc6f81d1b95854867d78625949d56e477f8b7c
ldoyle0 <ldoyle0@gmail.com>
2013-09-01 00:51:05 +0000

    Initial add of files

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+var buf = require('buffer')
+
+var app = express.createServer(express.logger());
+var output = fs.readFileSync('index.html').toString(); 
+
+app.get('/', function(request, response) {
+    response.send(output);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});					
+
+
+ 				
