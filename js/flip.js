70195943ba26ec762413b6e234cdfa746bb52b9e
Lee Doyle <ldoyle0@gmail.com>
2014-04-25 01:46:30 +0000

    replaced the old images with smaller ones. Updated the content of each section, and changed the alignment of everything.

--- js/flip.js
+++
@@ -1,6 +0,0 @@
- $('.flip').click(function(){
-        $(this).find('.card').addClass('flipped').mouseleave(function(){
-            $(this).removeClass('flipped');
-        });
-        return false;
-    });
